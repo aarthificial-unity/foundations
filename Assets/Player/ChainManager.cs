679fa04b6fb32adf522a181e1ede034624149e6e
aarthificial <aarthificial@gmail.com>
2023-06-15 21:59:16 +0200

    feat: point and click prototype

---
+++ Assets/Player/ChainManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player {
+  public class ChainManager : MonoBehaviour {
+    [SerializeField] private GameObject _link;
+    [SerializeField] private int _length;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _linkLength = 1;
+
+    [SerializeField] private Rigidbody _from;
+    [SerializeField] private Rigidbody _to;
+
+    private Rigidbody[] _links;
+
+    private void Awake() {
+      _links = new Rigidbody[_length];
+      var prev = Instantiate(_link);
+      prev.transform.position = _from.position;
+      _links[0] = prev.GetComponent<Rigidbody>();
+
+      var joint = _to.GetComponent<Joint>();
+      joint.connectedBody = _links[0];
+      var anchor = joint.connectedAnchor;
+      anchor.y = -_linkLength;
+      joint.connectedAnchor = anchor;
+      anchor = joint.anchor;
+      anchor.y = _linkLength;
+      joint.anchor = anchor;
+
+      for (var i = 1; i < _length; i++) {
+        var link = Instantiate(_link);
+        link.transform.position = _from.position + _offset * i;
+        joint = prev.GetComponent<Joint>();
+        _links[i] = link.GetComponent<Rigidbody>();
+        joint.connectedBody = _links[i];
+        anchor = joint.connectedAnchor;
+        anchor.y = -_linkLength;
+        joint.connectedAnchor = anchor;
+        anchor = joint.anchor;
+        anchor.y = _linkLength;
+        joint.anchor = anchor;
+
+        if (i % 2 == 0) {
+          joint.transform.GetChild(0).Rotate(0, 90, 0);
+        }
+
+        prev = link;
+      }
+
+      Destroy(prev.GetComponent<Joint>());
+      joint = _from.GetComponent<Joint>();
+      joint.connectedBody = prev.GetComponent<Rigidbody>();
+      anchor = joint.connectedAnchor;
+      anchor.y = _linkLength;
+      joint.connectedAnchor = anchor;
+    }
+  }
+}
---
+++ Assets/Player/PlayerManager.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace Player {
+  public class PlayerManager : MonoBehaviour {
+    [SerializeField] private Camera _camera;
+    [SerializeField] private InputActionReference _targetAction;
+    [SerializeField] private InputActionReference _commandAction;
+    [SerializeField] private float _acceleration = 30;
+    [SerializeField] private float _minDistance = 4;
+    [SerializeField] private float _maxDistance = 5;
+
+    [SerializeField] private PlayerController _lt;
+    [SerializeField] private PlayerController _rt;
+    [SerializeField] private GameObject _target;
+    [SerializeField] private Material _ltMaterial;
+    [SerializeField] private Material _rtMaterial;
+
+    private Vector3? _commandPosition;
+    private bool _isFollowing;
+    private bool _isCommandingLt;
+
+    private void Awake() {
+      _lt.Agent.acceleration = _acceleration;
+      _rt.Agent.acceleration = _acceleration;
+      _target.SetActive(false);
+    }
+
+    private void Update() {
+      var commanding = _commandAction.action.ReadValue<float>();
+      if (commanding != 0) {
+        FindCommandPosition();
+        _isCommandingLt = commanding > 0;
+        _target.GetComponent<MeshRenderer>().material =
+          _isCommandingLt ? _ltMaterial : _rtMaterial;
+      }
+
+      if (_commandPosition.HasValue) {
+        _target.SetActive(true);
+        var command = _isCommandingLt ? _lt : _rt;
+        var follow = _isCommandingLt ? _rt : _lt;
+        command.Agent.destination = _commandPosition.Value;
+        Debug.DrawRay(_commandPosition.Value, Vector3.up, Color.red);
+        command.Agent.stoppingDistance = 0;
+        command.Agent.acceleration = _acceleration;
+        follow.Agent.acceleration = _acceleration / 2;
+        follow.Agent.destination = command.transform.position;
+        if (follow.Agent.remainingDistance < _minDistance) {
+          _isFollowing = false;
+        }
+
+        if (follow.Agent.remainingDistance > _maxDistance) {
+          _isFollowing = true;
+        }
+
+        if (_isFollowing) {
+          follow.Agent.stoppingDistance = _minDistance;
+        } else {
+          follow.Agent.stoppingDistance = _maxDistance;
+        }
+
+        if (!command.Agent.pathPending
+          && command.Agent.remainingDistance < 0.001) {
+          _target.SetActive(false);
+        }
+      } else {
+        _lt.Agent.destination = _lt.transform.position;
+        _rt.Agent.destination = _rt.transform.position;
+      }
+    }
+
+    private void FindCommandPosition() {
+      var ray =
+        _camera.ScreenPointToRay(_targetAction.action.ReadValue<Vector2>());
+      if (Physics.Raycast(
+          ray,
+          out var hit,
+          100,
+          ~LayerMask.NameToLayer("Ground")
+        )
+        && NavMesh.SamplePosition(
+          hit.point,
+          out var navHit,
+          5,
+          NavMesh.AllAreas
+        )) {
+        _commandPosition = navHit.position;
+        _target.SetActive(true);
+        _target.transform.position = _commandPosition.Value;
+      } else {
+        _commandPosition = null;
+        _target.SetActive(false);
+      }
+    }
+  }
+}
