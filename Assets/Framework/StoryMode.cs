c79a8e4ed2d112b926da6133f9ea9bb88412f3a3
Jacob <64662184+aarthificial@users.noreply.github.com>
2023-06-15 22:06:30 +0200

    feat: point and click prototype (#8)

--- Assets/Framework/StoryMode.cs
+++ Assets/Framework/StoryMode.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Input;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Utils;
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace Player {
+  public class PlayerController : MonoBehaviour {
+    public NavMeshAgent Agent;
+  }
+}
