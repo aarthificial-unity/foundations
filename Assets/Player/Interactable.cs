aa4f0993112d594446bc403a124774e07270e158
Jacob <64662184+aarthificial@users.noreply.github.com>
2023-06-18 01:34:19 +0200

    feat: interactions and restricted movement (#9)

---
+++ Assets/Player/Interactable.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Player {
+  public class Interactable : MonoBehaviour {
+    private PlayerController _player;
+    private MeshRenderer _meshRenderer;
+    private Material _defaultMaterial;
+
+    private void Awake() {
+      _meshRenderer = GetComponent<MeshRenderer>();
+      _defaultMaterial = _meshRenderer.material;
+    }
+
+    public virtual Vector3 GetPosition() {
+      return transform.position;
+    }
+
+    public virtual float GetRadius() {
+      return 0.3f;
+    }
+
+    public virtual void StopInteraction(PlayerController player) {
+      Assert.AreEqual(
+        player,
+        _player,
+        "Stop was called by a different player than Start"
+      );
+      _player = null;
+      _meshRenderer.material = _defaultMaterial;
+    }
+
+    public virtual void SetReached(PlayerController player, bool hasReached) {
+      Assert.AreEqual(
+        player,
+        _player,
+        "InRange was called by a different player than Start"
+      );
+      _meshRenderer.material = hasReached ? player.Material : _defaultMaterial;
+    }
+
+    public virtual void StartInteraction(PlayerController player) {
+      Assert.IsNull(
+        _player,
+        "Trying to start interaction while already interacting"
+      );
+      _player = player;
+    }
+  }
+}
---
+++ Assets/Player/PlayerConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player {
+  public class PlayerConfig : ScriptableObject {
+    [NonSerialized] public int GroundLayer;
+    public LayerMask InteractionMask;
+    public float Acceleration = 30;
+    public float WalkSpeed = 5;
+    public float SprintSpeed = 10;
+    public float MinDistance = 4;
+    public float MaxDistance = 5;
+    public float LimitDistance = 6;
+    public float DoubleTapTime = 0.75f;
+
+    private void OnEnable() {
+      GroundLayer = LayerMask.NameToLayer("Ground");
+    }
+  }
+}
