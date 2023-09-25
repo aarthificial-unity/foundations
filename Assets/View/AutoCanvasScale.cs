b78acb6143cbb5f0cc539388674654bb38e0b282
aarthificial <aarthificial@gmail.com>
2023-09-26 01:19:41 +0200

    fix: pause chain sfx and fix version scaling

---
+++ Assets/View/AutoCanvasScale.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace View {
+  [DefaultExecutionOrder(-300)]
+  [RequireComponent(typeof(Canvas))]
+  public class AutoCanvasScale : MonoBehaviour {
+    private Canvas _canvas;
+
+    private void Awake() {
+      _canvas = GetComponent<Canvas>();
+    }
+
+    private void Update() {
+      var ratio = Screen.width / (float)Screen.height;
+      if (ratio > 16 / 9f) {
+        _canvas.scaleFactor = Screen.height / 1080f;
+      } else {
+        _canvas.scaleFactor = Screen.width / 1920f;
+      }
+    }
+  }
+}
