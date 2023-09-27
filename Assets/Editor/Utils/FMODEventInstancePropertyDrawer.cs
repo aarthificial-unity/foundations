3dfdb9b35bef2a4ce435ae515be06db5fbec20f3
Jacob <64662184+aarthificial@users.noreply.github.com>
2023-09-27 03:41:25 +0200

    feat: configure audio for dialogue interface (#52)

---
+++ Assets/Editor/Utils/FMODEventInstancePropertyDrawer.cs
@@ -0,0 +1,9 @@
+﻿using Audio;
+using UnityEditor;
+
+namespace Editor.Utils {
+  [CustomPropertyDrawer(typeof(FMODEventInstance))]
+  public class FMODEventInstancePropertyDrawer : WrapperPropertyDrawer {
+    protected override string WrappedName => nameof(FMODEventInstance.Event);
+  }
+}
---
+++ Assets/Editor/Utils/WrapperPropertyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Utils;
+
+namespace Editor.Utils {
+  public abstract class WrapperPropertyDrawer : PropertyDrawer {
+    protected virtual string WrappedName { get; }
+
+    public override void OnGUI(
+      Rect position,
+      SerializedProperty property,
+      GUIContent label
+    ) {
+      var wrappedProperty = property.FindPropertyRelative(WrappedName);
+      EditorGUI.PropertyField(position, wrappedProperty, label, true);
+    }
+  }
+}
