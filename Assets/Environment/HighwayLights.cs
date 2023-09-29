59ef0d45881421e4124ca844a7e2aaa304d873b1
aarthificial <aarthificial@gmail.com>
2023-09-29 02:27:43 +0200

    feat: add rocks and barriers to entry gate

---
+++ Assets/Environment/HighwayLights.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Environment {
+  public class HighwayLights : MonoBehaviour {
+    [SerializeField] private Vector2 _speed;
+    [SerializeField] private float _fluctuation;
+
+    private float _angle;
+
+    private void Awake() {
+      _angle = transform.localRotation.eulerAngles.y;
+    }
+
+    private void Update() {
+      var speed = Mathf.Sin(Time.time * _fluctuation)
+        .Remap(-1, 1, _speed.x, _speed.y);
+      _angle += speed * Time.deltaTime;
+      transform.localRotation = Quaternion.Euler(0, _angle, 0);
+    }
+  }
+}
