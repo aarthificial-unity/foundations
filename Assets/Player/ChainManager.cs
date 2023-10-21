2a34795a5b599ece7e0ac3c08188101b137f2b7d
John Janigan-Mills <cosmicmonkeysounds@gmail.com>
2023-10-21 14:44:29 -0300

    chain update, added vcas

--- Assets/Player/ChainManager.cs
+++ Assets/Player/ChainManager.cs
@@ -1,5 +1,8 @@
 ﻿using Audio;
+using Player.Surfaces;
 using UnityEngine;
+using Utils;
+using Utils.Tweening;
 
 namespace Player {
   [DefaultExecutionOrder(2)]
@@ -12,10 +15,17 @@ namespace Player {
     [SerializeField] private Quaternion[] _rotations;
 
     [SerializeField] private FMODEventInstance _chainEvent;
-    [SerializeField] private FMODParameter _chainAccelerationParam;
+    [SerializeField] private FMODParameter _chainVelocityParam, _chainAccelerationParam, _chainSurfaceParam;
 
     private Rigidbody[] _links;
+    private Rigidbody _middleLink;
     private Vector3 _prevFrameVelocity;
+
+    private SpringTween _velocityTween;
+    private SpringTween _accelTween;
+    [SerializeField] private SpringConfig _velocitySpringConfig;
+    [SerializeField] private SpringConfig _accelSpringConfig;
+    [Inject] public PlayerConfig Config;
 
     private void Awake() {
 #if UNITY_EDITOR
@@ -67,6 +77,8 @@ namespace Player {
       anchor = joint.connectedAnchor;
       anchor.y = _linkLength;
       joint.connectedAnchor = anchor;
+
+      _middleLink = _links[_length / 2];
     }
 
     private void Start() {
@@ -77,20 +89,36 @@ namespace Player {
       }
       _prevFrameVelocity = Vector3.zero;
       _chainEvent.Setup();
-      _chainEvent.AttachToGameObject(this.gameObject);
+      _chainEvent.AttachToGameObject(_middleLink.gameObject);
+      _chainEvent.SetParameter(_chainVelocityParam, 0.0f);
       _chainEvent.SetParameter(_chainAccelerationParam, 0.0f);
       _chainEvent.Play();
     }
 
     private void FixedUpdate() {
-      _chainEvent.Update3DPosition();
       Vector3 currentVelocity = Vector3.zero;
       for (var i = 0; i < _length; i++) {
         currentVelocity += _links[i].velocity;
       }
       Vector3 accel = currentVelocity - _prevFrameVelocity;
       _prevFrameVelocity = currentVelocity;
-      _chainEvent.SetParameter(_chainAccelerationParam, accel.magnitude);
+      _velocityTween.Set(currentVelocity.magnitude);
+      _velocityTween.FixedUpdate(_velocitySpringConfig);
+      _accelTween.Set(accel.magnitude);
+      _accelTween.FixedUpdate(_accelSpringConfig);
+      _chainEvent.Update3DPosition();
+      _chainEvent.SetParameter(_chainVelocityParam, _velocityTween.X);
+      _chainEvent.SetParameter(_chainAccelerationParam, _accelTween.X);
+      if (Physics.Raycast(
+          _middleLink.position,
+          Vector3.down,
+          out var hit,
+          2f,
+          Config.GroundMask
+        )
+        && hit.collider.TryGetComponent<ISurfaceProvider>(out var surface)) {
+        _chainEvent.SetParameter(_chainSurfaceParam, surface.GetSurface(hit));
+      }
     }
 
 #if UNITY_EDITOR
